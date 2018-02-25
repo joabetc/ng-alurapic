e26889feded73244603fe9820711e45e4a101aed
Joabe Costa <joabetc@hotmail.com>
2018-02-25 15:04:03 -0300

    Creating routes for the SPA

---
+++ client/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { RegistrationComponent } from './registration/resgistration.component';
+import { ListComponent } from './list/list.component';
+
+const appRoutes: Routes = [
+    { path: '', component: ListComponent },
+    { path: 'registration', component: RegistrationComponent },
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
