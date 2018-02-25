cf89aa8295e05e3ddda366317e50f63f56827680
Joabe Costa <joabetc@hotmail.com>
2018-02-25 11:59:06 -0300

    Importing HttpModule

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
-    imports: [ BrowserModule, PhotoModule ],
+    imports: [ BrowserModule, PhotoModule, HttpModule ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 })
