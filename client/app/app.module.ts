caa788dc10a2ee9665e6a3d44ff3021801b396b3
Joabe Costa <joabetc@hotmail.com>
2018-02-25 12:18:09 -0300

    Importing RxJs Map operator

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 import { AppComponent } from './app.component';
 import { PhotoModule } from './photo/photo.module';
