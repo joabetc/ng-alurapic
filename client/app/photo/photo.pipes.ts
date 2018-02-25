fc3c31b7c8eb9ab040872a34f35ba7be2d422a1e
Joabe Costa <joabetc@hotmail.com>
2018-02-25 15:38:19 -0300

    Implementing Pipe logic

--- client/app/photo/photo.pipes.ts
+++ client/app/photo/photo.pipes.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class TitleFilter implements PipeTransform {
     
     transform(photos, filter) {
-
+        return photos.filter(photo => photo.titulo.toLowerCase().includes(filter.toLowerCase()));
     }
 }
