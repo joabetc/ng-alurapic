5696a5744813f664cd359732be1f10ae482de798
Joabe Costa <joabetc@hotmail.com>
2018-02-26 22:18:06 -0300

    Sending data to the server

--- client/app/registration/resgistration.component.ts
+++ client/app/registration/resgistration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { PhotoComponent } from "../photo/photo.component";
+import { Http, Headers } from "@angular/http";
 
 @Component({
     moduleId: module.id,
@@ -9,8 +10,21 @@ import { PhotoComponent } from "../photo/photo.component";
 export class RegistrationComponent {
 
     photo: PhotoComponent = new PhotoComponent();
+    http: Http;
+
+    constructor(http: Http) {
+        this.http = http;
+    }
 
     insert(event) {
         event.preventDefault();
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http
+            .post('v1/fotos', JSON.stringify(this.photo), {headers: headers} )
+            .subscribe(() => {
+                console.log("Photo successfully saved");
+            });
     }
 }
